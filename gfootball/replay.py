77818c43c1f669ff77b65c892d37314c03fa4cde
anjakklein <anja.klein@lingualytica.de>
2020-03-18 17:33:01 +0100

    fix example usage

--- gfootball/replay.py
+++ gfootball/replay.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Kommentar
 
 """Script allowing to replay a given trace file.
    Example usage:
